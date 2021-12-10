77483453df67b9a4d78172b9161ef4a9126bb2be
Karthik <dkkarthik2000@gmail.com>
2021-12-10 18:42:56 +0530

    update in inputfield

--- src/components/inputfield.js
+++ src/components/inputfield.js
@@ -6,17 +6,20 @@ import { useNavigate } from "react-router"
 const apikey = `2debe0f00b477f3d87075013e384ea67`
 
 
-const Input = ({ type, name, placeholder, onchange }) => {
+const Input = ({ type, name, placeholder }) => {
     const [moviename, getmoviename] = React.useState('')
     const [movielist, getmovielist] = React.useState([])
     const naviagte = useNavigate()
 
     const handletextfield = (event) => {
+
         getmoviename(event.target.value);
 
     }
     const suggestiononclick = ({ id, name, title }) => {
-        document.querySelector(".suggestion").classList.toggle("show")
+        document.querySelector(".suggestion").classList.toggle("hide")
+        console.log(document.querySelector(".suggestion"));
+
         naviagte(`../moviedetail/${name ? name : title}/${id}`)
     }
 
@@ -30,17 +33,24 @@ const Input = ({ type, name, placeholder, onchange }) => {
     }
 
     React.useEffect(() => {
-        if (moviename.length !== 0)
+        if (moviename.length !== 0 )
             searchmovie(moviename)
-    }, [moviename])
+        else {
+            let temp = movielist
+            temp.length = 0
+            getmovielist(temp)
+        }
+
+    }, [moviename, movielist])
 
 
     return (
         <React.Fragment>
             <input type={type} name={name} placeholder={placeholder} onKeyUp={handletextfield} />
-            <div className={`suggestion ${moviename.length !== 0 ? `show` : `hide`}`}   >
-                {moviename !== '' ?
-                    movielist.map(ele => ele.backdrop_path ?
+            {movielist.length !== 0 ?
+                <div className={`suggestion`}  >
+
+                    {movielist.map(ele => ele.backdrop_path ?
                         <div key={ele.id} onClick={() => suggestiononclick(
                             {
                                 id: ele.id,
@@ -49,13 +59,17 @@ const Input = ({ type, name, placeholder, onchange }) => {
                             }
                         )} >
                             <img src={`https://image.tmdb.org/t/p/w500${ele.backdrop_path}`} alt={ele.name ? ele.name : ele.title} />
-                            <h2>{ele.name ? ele.name : ele.title}</h2></div>
-                        : null
+                            <h2>{ele.name ? ele.name : ele.title}</h2>                        </div>
+                        : null)}
+                </div>
+                : document.querySelector(".suggestion")?.classList.add("hide")
+            }
 
-                    )
-                    : document.querySelector(".suggestion")?.classList.toggle("hide")
-                }
-            </div>
+
+
+
+
+
         </React.Fragment>
     )
 }
